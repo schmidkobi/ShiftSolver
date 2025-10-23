7359f1bf87f61c1c5e7c63acff7d365ac87bdb32
schmidkovic <milan.schmidle@stud.uni-heidelberg.de>
2025-10-23 22:21:12 +0200

    Init commit

---
+++ java/shiftplanner/src/main/java/com/sammyshifts/domain/Room.java
@@ -0,0 +1,22 @@
+package com.sammyshifts.domain;
+
+public class Room {
+
+    private String name;
+
+    public Room() {
+    }
+
+    public Room(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return name;
+    }
+}
